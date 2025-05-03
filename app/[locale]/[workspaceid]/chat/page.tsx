a04b13fc08352801b202ba45f1a3604f54a7c3af
kicko <kicko2002@gmail.com>
2025-05-03 02:40:31 -0500

    KlynoAIContext fixed pt 2 renaming

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -7,7 +7,7 @@ import { ChatSettings } from "@/components/chat/chat-settings"
 import { ChatUI } from "@/components/chat/chat-ui"
 import { QuickSettings } from "@/components/chat/quick-settings"
 import { Brand } from "@/components/ui/brand"
-import { ChatbotUIContext } from "@/context/context"
+import { KlynoAIContext } from "@/context/context"
 import useHotkey from "@/lib/hooks/use-hotkey"
 import { useTheme } from "next-themes"
 import { useContext } from "react"
@@ -18,7 +18,7 @@ export default function ChatPage() {
     handleFocusChatInput()
   })
 
-  const { chatMessages } = useContext(ChatbotUIContext)
+  const { chatMessages } = useContext(KlynoAIContext)
 
   const { handleNewChat, handleFocusChatInput } = useChatHandler()
 
