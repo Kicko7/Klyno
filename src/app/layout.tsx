31b3801aee6c7697fe3319883f7e0da996524f65
kicko <kicko2002@gmail.com>
2025-07-01 01:36:12 -0500

    fixed nuqs adapterand using 2.4.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import React, { ReactNode } from 'react';
 
 import AuthProvider from '@/layout/AuthProvider';
 import GlobalProvider from '@/layout/GlobalProvider';
@@ -6,13 +7,15 @@ import GlobalProvider from '@/layout/GlobalProvider';
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   // TODO: Replace these defaults with real user/cookie/device detection if needed
   return (
     <GlobalProvider appearance="dark" isMobile={false} locale="en">
       <AuthProvider>
         <html lang="en">
-          <body>{children}</body>
+          <body>
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </body>
         </html>
       </AuthProvider>
     </GlobalProvider>
