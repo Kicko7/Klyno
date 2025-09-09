76028ae221f2e03bf322ff784acb7c1761f1bbac
Masood <masooddulrehman@gmail.com>
2025-09-09 12:00:49 +0500

    Toggle Workspace Team Chat Button Function called

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -7,7 +7,7 @@ export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 export const BRANDING_NAME = 'Klyno AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Klyno AI';
 
 export const BRANDING_URL = {
   help: undefined,
